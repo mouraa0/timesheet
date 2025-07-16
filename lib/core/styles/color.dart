c7229b29103277f485d5391fc912ad0bdcd3e7c9
mouraa0 <jpcmouraa@gmail.com>
2025-07-16 20:44:16 -0300

    feat: create AppColors

---
+++ lib/core/styles/color.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color secundaryContainer = Color(0x29FFFFFF);
+  static const Color onSecundaryContainer = Color(0xFFFFFFFF);
+
+  static const Color onSurface = Color(0xFFFFFFFF);
+}
