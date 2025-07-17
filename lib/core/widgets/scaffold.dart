4f06edfad868ee955b5316a9c02d0ec77a0b3b0b
mouraa0 <jpcmouraa@gmail.com>
2025-07-17 10:44:06 -0300

    feat: default AppScaffold

---
+++ lib/core/widgets/scaffold.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class AppScaffold extends StatelessWidget {
+  final Widget body;
+
+  const AppScaffold({super.key, required this.body});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(16),
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Color(0xFF0C1D4D), Color(0xFF214ECC)],
+          ),
+        ),
+        child: body,
+      ),
+    );
+  }
+}
