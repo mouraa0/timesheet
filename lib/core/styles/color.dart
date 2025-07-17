8a401507af94cfd4d4c8c4a3f5ee955b810e3539
mouraa0 <jpcmouraa@gmail.com>
2025-07-17 10:43:27 -0300

    style: add new colors

--- lib/core/styles/color.dart
+++ lib/core/styles/color.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
+  static const Color primaryContainer = Color(0xFFFFFFFF);
+  static const Color onPrimaryContainer = Color(0xFF000000);
+
   static const Color secundaryContainer = Color(0x29FFFFFF);
   static const Color onSecundaryContainer = Color(0xFFFFFFFF);
 
   static const Color onSurface = Color(0xFFFFFFFF);
+  static const Color surfaceAtOne = Color(0x14FFFFFF);
 }
