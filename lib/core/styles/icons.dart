e5e9115f76cb20df07610a5ceac585ca7efc4e0b
mouraa0 <jpcmouraa@gmail.com>
2025-07-17 10:43:39 -0300

    chore: add new icons

--- lib/core/styles/icons.dart
+++ lib/core/styles/icons.dart
@@ -1,4 +1,8 @@
 class AppIcons {
   static const String order = 'assets/icons/order.svg';
   static const String plus = 'assets/icons/plus.svg';
+  static const String bag = 'assets/icons/bag.svg';
+  static const String star = 'assets/icons/star.svg';
+  static const String clock = 'assets/icons/clock.svg';
+  static const String pause = 'assets/icons/pause.svg';
 }
